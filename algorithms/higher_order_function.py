39537a80b89a72231c837fb3981620e3c1f3b431
dhruvmanila <dhruvmanila@gmail.com>
2020-08-14 08:55:09 +0530

    Added additional operators and ability for terminal usage

--- algorithms/higher_order_function.py
+++ algorithms/higher_order_function.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Higher order function operators
+Higher order function operators:
+This is just for understanding about higher order functions and
+what is possible. It can also be used from the terminal.
 
 Usage:
 >>> eight(times(seven()))
@@ -19,15 +21,19 @@ Usage:
 >>> four(times(zero()))
 0
 
->>> nine(divided_by(nine()))
-1.0
+>>> nine(modulus(eight()))
+1
 
 >>> one(divided_by(three()))
 0.3333333333333333
 
->>> five(times(five()))
+>>> five(raise_to(five()))
 25
 
+Terminal Usage:
+python higher_order_function.py <number> <operator> <number>
+- number should be in words
+- Supported operators: +, -, x, /, %, ^
 """
 
 def zero(f=None):
@@ -72,3 +78,40 @@ def times(y):
 
 def divided_by(y):
     return lambda x: x / y
+
+def raise_to(y):
+    return lambda x: x**y
+
+def modulus(y):
+    return lambda x: x % y
+
+
+if __name__ == '__main__':
+    import sys
+
+    OPERATOR = {
+        '+': 'plus',
+        '-': 'minus',
+        'x': 'times',
+        '/': 'divided_by',
+        '%': 'modulus',
+        '^': 'raise_to'
+    }
+
+    try:
+        args = sys.argv[1:4]
+        num1, sign, num2 = map(lambda x: x.lower(), args)
+        if any(val.isdecimal() for val in args):
+            raise TypeError("Numbers should be in words. Eg., one, two, etc.")
+        elif sign not in OPERATOR:
+            raise TypeError(f"Supported operators: {', '.join(OPERATOR)}")
+
+        oper = OPERATOR[sign]
+        result = eval(num1 + '(' + oper + '(' + num2 + '()))')
+        print(f"{num1} {sign} {num2} = {result}")
+
+    except TypeError as err:
+        print("Error:",err)
+    except ValueError:
+        print("Usage: "
+            "python higher_order_function.py <number> <operator> <number>")
